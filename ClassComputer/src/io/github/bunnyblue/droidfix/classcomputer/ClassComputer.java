c702e00ac9200d531fa8c9c7d02b544ea89d2090
BunnyBlue <bunnyblueair@gmail.com>
2015-11-10 18:55:55 +0800

    add subproject

---
+++ ClassComputer/src/io/github/bunnyblue/droidfix/classcomputer/ClassComputer.java
@@ -0,0 +1,10 @@
+package io.github.bunnyblue.droidfix.classcomputer;
+
+/**
+ * Created by BunnyBlue on 11/10/15.
+ */
+public class ClassComputer {
+    public static  void main(String[]args){
+        
+    }
+}
---
+++ droidfixcore/src/main/java/io/github/bunnyblue/droidfixcore/READme.java
@@ -0,0 +1,7 @@
+package io.github.bunnyblue.droidfixcore;
+
+/**
+ * Created by BunnyBlue on 11/9/15.
+ */
+public class READme {
+}
